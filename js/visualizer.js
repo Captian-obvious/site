716b97a261b16fa80ea07a57689abf27e5cc742e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-16 08:11:01 -0400

    Update visualizer.js

--- js/visualizer.js
+++ js/visualizer.js
@@ -68,7 +68,7 @@ window.onload = function() {
                 var b = 50;
                 
                 ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-                ctx.fillRect(x, HEIGHT - (barHeight/5), barWidth, barHeight/5);
+                ctx.fillRect(x, maxHeight + HEIGHT - (barHeight), barWidth, barHeight);
                         
                 x += barWidth + 1;
             }
