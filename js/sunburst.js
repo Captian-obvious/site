84ed714038614f8ad5f09c91a1a25d3c2b02e165
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-16 15:58:13 -0400

    Update sunburst.js

--- js/sunburst.js
+++ js/sunburst.js
@@ -1,6 +1,6 @@
 const button1 = document.getElementById("button1")
 let audio1 = new Audio()
-audio1.src = '../../MP3/tobu/Hope.mp3'
+audio1.src = button1.getAttribute("data-mediathumb-url")
 button1.addEventListener("click", function(){
   console.log('click');
   
