f231db4c127454bfc0185ee6396f776ae456fe24
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-16 16:20:21 -0400

    Update sunburst.js

--- js/sunburst.js
+++ js/sunburst.js
@@ -1,10 +1,9 @@
 const button = document.getElementById("MediaPlayerIcon-icon-play")
 const position = document.getElementById("time-position")
 const dur = document.getElementById("")
-let audio1 = new Audio()
+var audio1 = new Audio()
 audio1.src = button1.getAttribute("data-mediathumb-url")
 button.addEventListener("click", function(){
-  var aud = document.getElementById("audio")
   if(this.className == 'MediaPlayerIcon-icon-pause'){
     this.className = "MediaPlayerIcon-icon-play";
     audio1.pause();
@@ -12,19 +11,19 @@ button.addEventListener("click", function(){
     this.className = "MediaPlayerIcon-icon-pause";
     audio1.play();
   };
-  aud.addEventListener("ended", function() {
+  audio1.addEventListener("ended", function() {
     button.className = "MediaPlayerIcon-icon-play";
   });
 });
 dur.addEventListener("change", function() {
-  audio.currentTime = dur.value
+  audio1.currentTime = dur.value
 })
 audio1.addEventListener("timeupdate", function() {
-  var curminutes = Math.floor(audio.currentTime/60)
-  var curseconds = Math.ceil(audio.currentTime-(curminutes*60))
-  var minutes = Math.floor(audio.duration/60)
-  var seconds = Math.ceil(audio.duration-(minutes*60))
-  dur.value=audio.currentTime; 
-  dur.max = audio.duration;
+  var curminutes = Math.floor(audio1.currentTime/60)
+  var curseconds = Math.ceil(audio1.currentTime-(curminutes*60))
+  var minutes = Math.floor(audio1.duration/60)
+  var seconds = Math.ceil(audio1.duration-(minutes*60))
+  dur.value=audio1.currentTime; 
+  dur.max = audio1.duration;
   position.innerHTML = curminutes+":"+curseconds+" / "+minutes+":"+seconds
 });
