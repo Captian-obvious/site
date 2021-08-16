d66e0f5adfcb1d49724b6a9f6cc7d8a72fd6a393
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-16 16:12:00 -0400

    Update sunburst.js

--- js/sunburst.js
+++ js/sunburst.js
@@ -1,12 +1,16 @@
-const button1 = document.getElementById("MediaPlayerIcon-icon-play")
+const button = document.getElementById("MediaPlayerIcon-icon-play")
 let audio1 = new Audio()
 audio1.src = button1.getAttribute("data-mediathumb-url")
-button1.addEventListener("click", function(){
-  if button1.className == "MediaPlayerIcon icon-play" {
-    button1.className = "MediaPlayerIcon icon-pause
-    audio1.play()
+button.addEventListener("click", function(){
+  var aud = document.getElementById("audio")
+  if(this.className == 'MediaPlayerIcon-icon-pause'){
+    this.className = "MediaPlayerIcon-icon-play";
+    audio1.pause();
   }else{
-    button1.className = "MediaPlayerIcon icon-play"
-    audio1.pause()
-  }
+    this.className = "MediaPlayerIcon-icon-pause";
+    audio1.play();
+  };
+  aud.addEventListener("ended", function() {
+    button.className = "MediaPlayerIcon-icon-play";
+  });
 });
