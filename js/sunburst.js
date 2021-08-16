09419300a0b03241ca3c4f4ac1cbed92c8926fdd
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-16 16:19:10 -0400

    Update sunburst.js

--- js/sunburst.js
+++ js/sunburst.js
@@ -1,4 +1,6 @@
 const button = document.getElementById("MediaPlayerIcon-icon-play")
+const position = document.getElementById("time-position")
+const dur = document.getElementById("")
 let audio1 = new Audio()
 audio1.src = button1.getAttribute("data-mediathumb-url")
 button.addEventListener("click", function(){
@@ -13,4 +15,16 @@ button.addEventListener("click", function(){
   aud.addEventListener("ended", function() {
     button.className = "MediaPlayerIcon-icon-play";
   });
+});
+dur.addEventListener("change", function() {
+  audio.currentTime = dur.value
+})
+audio1.addEventListener("timeupdate", function() {
+  var curminutes = Math.floor(audio.currentTime/60)
+  var curseconds = Math.ceil(audio.currentTime-(curminutes*60))
+  var minutes = Math.floor(audio.duration/60)
+  var seconds = Math.ceil(audio.duration-(minutes*60))
+  dur.value=audio.currentTime; 
+  dur.max = audio.duration;
+  position.innerHTML = curminutes+":"+curseconds+" / "+minutes+":"+seconds
 });
