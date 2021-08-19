5a17fd198ed2a09103358ede55caba5aa14382cb
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-18 20:23:07 -0400

    Update visualizer.js

--- js/visualizer.js
+++ js/visualizer.js
@@ -83,11 +83,13 @@ window.onload = function() {
                 var h = HEIGHT / 2 + loud * barHeight * Math.sin(angle)
                 var w = WIDTH / 2 + loud * barWidth * Math.cos(angle)
                 
-                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-                ctx.fillRect(x, maxHeight + HEIGHT - barHeight, barWidth, barHeight)
                 ctx.beginPath()
                 ctx.strokeStyle = "rgb(" + r + "," + g + "," + b + ")"
-                ctx.lineTo(w,h - maxHeight)
+                ctx.moveTo(centerX, centerY)
+                ctx.lineTo(w,h + loud - maxHeight)
+                ctx.stroke()
+                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+                ctx.fillRect(x, maxHeight + HEIGHT - barHeight, barWidth, barHeight)
                 
                 x += barWidth + 1;
             }
