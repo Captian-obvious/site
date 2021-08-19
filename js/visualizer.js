fa48fe3baffd1448337ed6ee42af942918beb1fa
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-19 08:36:51 -0400

    Update visualizer.js

--- js/visualizer.js
+++ js/visualizer.js
@@ -80,7 +80,7 @@ window.onload = function() {
                 var b = 50;
                 var angle = angle_step * i
                 
-                var h = HEIGHT / 2 + loud/5 * barHeight * Math.sin(angle)
+                var h = HEIGHT / 2 + loud/5 * barHeight/2 * Math.sin(angle)
                 var w = WIDTH / 2 + loud/5 * barWidth * Math.cos(angle)
                 
                 ctx.beginPath()
