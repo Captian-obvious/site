bd7a103e2c7ce6d34ea51144bcd529904b83af6c
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-08-19 13:37:00 -0400

    Update visualizer.js

--- js/visualizer.js
+++ js/visualizer.js
@@ -79,8 +79,8 @@ window.onload = function() {
                 var x = WIDTH/2 + loud/5 * Math.cos(angle)
                 
                 
-                var y1 = HEIGHT / 2 +loud/5 * barHeight * Math.sin(angle)
-                var x1 = WIDTH / 2 +loud/5 * barHeight * Math.cos(angle)
+                var y1 = HEIGHT / 2 + loud/5 * barHeight/10 * Math.sin(angle)
+                var x1 = WIDTH / 2 + loud/5 * barHeight/10 * Math.cos(angle)
                 
                 ctx.beginPath()
                 ctx.moveTo(x, y)
