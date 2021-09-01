9cbe7c54a2933f0297378f05d9dd936a7dc8f0af
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-09-01 17:07:09 -0400

    Update visualizer.js

--- js/visualizer.js
+++ js/visualizer.js
@@ -105,7 +105,7 @@ window.onload = function() {
                var b = 50;
 
                ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-               ctx.fillRect(0,0+rad, barWidth, barHeight/5)
+               ctx.fillRect(0,0+rad, barWidth, barHeight/10)
                
                ctx.restore()
             }
