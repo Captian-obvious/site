60f3b895f86849cb9eee3ed71a9803515ff22643
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-09-21 09:42:53 -0400

    Update visualizer.js

--- js/visualizer.js
+++ js/visualizer.js
@@ -1,5 +1,5 @@
 const id4 = window.jsmediatags
-const meydaNode = window.meydaAnalyser
+const meydaNode = window.MeydaAnalyser
 window.onload = function() {
     var file = document.getElementById("thefile");
     const canvasContainer = document.getElementById("CanvasContainer");
