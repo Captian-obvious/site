f96259d2c379ae008a3f922cab1d5240978f130d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-10-06 14:57:57 -0400

    Add files via upload

---
+++ js/minjs.js
@@ -0,0 +1,12 @@
+import { Meyda } from 'https://unpkg.com/meyda@5.2.2/dist/web/meyda.min.js'
+import { jsmediatags } from 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.js'
+var m = Meyda
+var jsmt = jsmediatags
+export class mjs {
+    id4 = jsmt,
+    meyda = m,
+}
+window.mjs = {
+    id4 = jsmt,
+    meyda = m,
+}
