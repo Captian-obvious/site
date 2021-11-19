e237a4d447e50e2de0419cb544c5abb30d3d2214
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-19 11:30:59 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -2,8 +2,6 @@ const ID3 = window.jsmediatags;
 const sin = Math.sin;
 const π = Math.PI;
 var urlPerameter1 = false;
-var location = document.location
-location.replace("?player=true")
 
 function ease(t) {
     return sin(t * π/2);
