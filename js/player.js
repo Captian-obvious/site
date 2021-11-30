2b2fe7605421754fbe4957a7e7565afdef7b900e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2021-11-30 11:11:30 -0500

    Update player.js

--- js/player.js
+++ js/player.js
@@ -102,7 +102,6 @@ window.addEventListener('load', function() {
                 const format = tag.tags.picture.format;
                 const title = tag.tags.title;
                 const artist = tag.tags.artist;
-                input = artist + " - " + title
                 if (data && format != null) {
                     let str = "";
                     for (var o=0;o<data.length;o++) {
