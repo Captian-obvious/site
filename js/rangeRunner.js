7c904d5d6c5479520609a499f6721fb88ce07a1d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-10-13 11:51:36 -0400

    Update rangeRunner.js

--- js/rangeRunner.js
+++ js/rangeRunner.js
@@ -8,7 +8,7 @@ for (var i=1; i <= rangeInputs.length; i++) {
             const max = target.max
             const val = target.value
 
-            target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%')
+            target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
         }
     })
 }
