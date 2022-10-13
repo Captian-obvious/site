eeec49d3b60c85883acf2cbf43fd9e43b4ff5eba
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2022-10-13 11:41:15 -0400

    Update rangeRunner.js

--- js/rangeRunner.js
+++ js/rangeRunner.js
@@ -1,14 +1,13 @@
-const rangeInputs = document.querySelectorAll('input[type="range"]')
-rangeInputs.forEach(input => {
+const rangeInputs = document.getElementsByTagName('input')
+for (var i=1; i <= rangeInputs.length; i++) {
     input.addEventListener('input', function(e) {
-        let target = e.target
-        if (target.type !== 'range') {
-            target = document.getElementById('range') || document.getElementById('MediaPlayerControl-seekbar')
-        } 
-        const min = target.min
-        const max = target.max
-        const val = target.value
+        if (input.type === 'range') {
+            let target = input
+            const min = target.min
+            const max = target.max
+            const val = target.value
 
-        target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+            target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%')
+        }
     })
-})
+}
